ea19e04e5c419b9ece5cc267acb00c616c308781
Mike 'Fuzzy' Partin <fuzzy@thwap.org>
2020-02-17 23:22:08 -0800

    just added comments so peeps know what I was up to

--- args.v
+++ args.v
@@ -1,9 +1,12 @@
 module main
 
+// a list of type flags for internal processing
 enum ArgType {
 	bool_t string_t int_t float_t
 }
 
+// our main processing structure, to be fully documented
+// at a later point.
 struct Arguments {
 pub:
 	arguments []Argumenter
@@ -13,13 +16,22 @@ pub:
 	homepage string
 }
 
+// a stub for an idea that I'm fleshing out, but probably
+// won't follow up on. We'll have to wait and see I guess.
 fn (self Arguments) process() { }
 
+
+// I went ahead and put an interface in here in case we
+// decide to impliment specific type structs and not have
+// a single meta type. It's called future-proofing yo!
 interface Argumenter {
 	do_match(arg string) bool
 	validate()
 }
 
+// the base argument structure, it's a rough draft of what
+// was in my head at the moment. Seems sane enough for the
+// most part though, so we'll see.
 struct BaseArg {
 mut:
 	result_v string
@@ -38,6 +50,10 @@ fn (self BaseArg) do_match(arg string) bool {
 }
 
 fn (self BaseArg) validate() { }
+
+// generator functions for all of our argument types
+// there isn't really a need to individually call these
+// out here.
 
 fn bool_arg(n string, a []string, h string) &BaseArg {
 	return &BaseArg{"", ArgType.bool_t, n, a, h}
