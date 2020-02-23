0ab9106b22221bfa51e82273ef2017589225925c
Mike 'Fuzzy' Partin <fuzzy@thwap.org>
2020-02-22 16:13:04 -0800

    added some parser code, still a wip so check back

--- args.v
+++ args.v
@@ -46,6 +46,36 @@ fn (self BaseArg) do_match(arg string) bool {
 	// this should parse several styles
 	// (bare|-short|--long)(=<str>|<int>|<float>) 
 	// (NOTE: This isn't supposed to be a valid regex)
+
+	// this will be the argument buffer
+	mut arg_b := ""
+
+	// now find the argument format, first check the
+	// simplest case, just a straight up match
+	if self.name == arg {
+		return true
+	// now see if it's a --long-argument style
+	} else if arg[0..1] == "--" {
+		arg_b = arg[2..]
+		if arg_b == self.name {
+			return true
+		} else if arg_b.contains("=") && arg_b.split("=")[0] == self.name {
+			return true
+		} else {
+			return false
+		}
+	// now see if it's a plan9 or gnu style short flag
+	} else if arg[0..1].contains("-") {
+		arg_b = arg[1..]
+	} else if arg.contains("=") && arg.split("=")[0] == self.name	{
+		arg_b = arg
+	// we've failed out of known styles to parse
+	} else {
+		return false
+	}
+
+	// if we've gotten this far, then we need to check for
+	// assignment type arguments
 	return false
 }
 
